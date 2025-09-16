e60ff38287e0d1f7a24ff6a39677a0b0eb31882f
yesicagomez118 <yesicagomez118@gmail.com>
2025-09-15 23:21:13 -0500

    comit creado

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 import streamlit as st
 
 st.header('Lanzar una moneda')
+chart = st.line_chart([0.5])
+
+def toss_coin(n): # función que emula el lanzamiento de una moneda
+
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+
+    for r in trial_outcomes:
+        outcome_no +=1
+        if r == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+
+    return mean
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
 start_button = st.button('Ejecutar')
 
